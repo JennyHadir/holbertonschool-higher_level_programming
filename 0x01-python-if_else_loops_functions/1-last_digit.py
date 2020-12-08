60eef0e3955d30e9b0216fa3e0aea462cf66c159
JennyHadir <hadirjenny@hotmail.com>
2020-12-08 15:28:27 +0100

    check6

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,5 +9,5 @@ if mod == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, mod))
 elif mod > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".format(number, mod))
-else:
+elif mod < 6 and mod != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, mod))
--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 != 0:
-        c = chr(i - 32)
-        print("{:s}".format(c), end = "")
+        print("{:s}".format(chr(i - 32)), end = "")
     else:
-        c = chr(i)
-        print("{:s}".format(c), end = "")
+        print("{:s}".format(chr(i)), end = "")
