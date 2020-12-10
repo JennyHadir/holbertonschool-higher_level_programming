8c8373f1bb655ccfd36f6a63c00f1c1689b7dc02
JennyHadir <hadirjenny@hotmail.com>
2020-12-10 13:01:08 +0100

    check6

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) - 1 == 0:
-    print("{:d} arguments.".format(len(sys.argv) - 1))
-if len(sys.argv) - 1 > 1:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-elif len(sys.argv) - 1 == 1:
-    print("{:d} argument:".format(1))
-for i in range (1, len(sys.argv)):
-    print("{:d}: {:s}".format(i, sys.argv[i]))
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) - 1 == 0:
+        print("{:d} arguments.".format(len(argv) - 1))
+    if len(argv) - 1 > 1:
+        print("{:d} arguments:".format(len(argv) - 1))
+    elif len(argv) - 1 == 1:
+        print("{:d} argument:".format(1))
+    for i in range (1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
