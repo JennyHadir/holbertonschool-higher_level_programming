30a92d655b29ab6d95b7f6335f224ce0269d8004
JennyHadir <hadirjenny@hotmail.com>
2020-12-10 13:07:20 +0100

    string format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,10 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) - 1 == 0:
-        print("{:d} arguments.".format(len(argv) - 1))
+        print("{} arguments.".format(len(argv) - 1))
     if len(argv) - 1 > 1:
-        print("{:d} arguments:".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
     elif len(argv) - 1 == 1:
-        print("{:d} argument:".format(1))
+        print("{} argument:".format(1))
     for i in range (1, len(argv)):
-        print("{:d}: {:s}".format(i, argv[i]))
-
+        print("{}: {}".format(i, argv[i]))
