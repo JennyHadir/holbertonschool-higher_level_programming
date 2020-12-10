f4876999de8bf75d0c19e4e39f92cebeb010bc25
JennyHadir <hadirjenny@hotmail.com>
2020-12-10 13:20:33 +0100

    pep 8 style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("{} arguments:".format(len(argv) - 1))
     elif len(argv) - 1 == 1:
         print("{} argument:".format(1))
-    for i in range (1, len(argv)):
+    for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
