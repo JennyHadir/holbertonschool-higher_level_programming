9bf74c3873bee4b8cdafec061bb6122d2ad9d4fa
JennyHadir <hadirjenny@hotmail.com>
2020-12-15 15:17:47 +0100

    [FIX] : pep style

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,5 @@ def delete_at(my_list=[], idx=0):
     if my_list:
         if idx >= len(my_list) or idx < 0:
             return my_list
-        else:
-            for rem in my_list:
-                if rem - 1 == idx:
-                    my_list.remove(rem)
-            return my_list
+        my_list.remove(my_list[idx])
+        return my_list
