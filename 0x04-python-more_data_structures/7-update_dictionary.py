7bbc4c18e7d4e455c99ef9b44488f9b6fce9bb06
JennyHadir <hadirjenny@hotmail.com>
2020-12-17 14:23:11 +0100

    [ADD]

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,6 @@ def update_dictionary(a_dictionary, key, value):
     for i in a_dictionary:
         if i == key:
             a_dictionary[key] = value
-    if key not in a_dictionary:
-        a_dictionary[key] = value
+        if key not in a_dictionary:
+            a_dictionary[key] = value
     return a_dictionary
