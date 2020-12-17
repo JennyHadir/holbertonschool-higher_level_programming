f23093b58cddead7b7753d71c8c7332ec84887d3
JennyHadir <hadirjenny@hotmail.com>
2020-12-17 14:16:05 +0100

    [ADD]

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+update_dictionary = __import__('7-update_dictionary').update_dictionary
 
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+for i in sorted(new_dict):
+        print("{}: {}".format(i, new_dict[i]))
+
+print("--")
+for i in sorted(a_dictionary):
+    print("{}: {}".format(i, a_dictionary[i]))
+
+print("--")
+print("--")
+
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+for i in sorted(new_dict):
+    print("{}: {}".format(i, new_dict[i]))
+
+print("--")
+for i in sorted(a_dictionary):
+    print("{}: {}".format(i, a_dictionary[i]))
