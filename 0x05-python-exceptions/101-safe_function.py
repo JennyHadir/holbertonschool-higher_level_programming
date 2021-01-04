f5a71042242899cee4861d1badf34103a2f46612
JennyHadir <hadirjenny@hotmail.com>
2021-01-04 14:54:17 +0100

    [ADD]: ADVANCED

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        result = None
+    return result
