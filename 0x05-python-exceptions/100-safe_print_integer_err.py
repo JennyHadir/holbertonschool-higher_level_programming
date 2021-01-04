f97603d84ab3084ec292b1f327efd859ad2f0633
JennyHadir <hadirjenny@hotmail.com>
2021-01-04 14:42:00 +0100

    [FIx]: pep8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,9 +2,8 @@
 def safe_print_integer_err(value):
     import sys
     try:
-       print("{:d}".format(value))
-    except (ValueError, TypeError):
-        print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
-    else:
-       return True
