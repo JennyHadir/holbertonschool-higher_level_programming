f6fa1157d261ad69a8d13b8cf454f5be02579a73
JennyHadir <hadirjenny@hotmail.com>
2021-01-12 23:26:35 +0100

    [ADD]

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(_list=[]):
+    _list += ["Holberton"]
+    return ", ".join(_list)
---
+++ 0x09-python-everything_is_object/101-locket_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Defines LockedClass """
+
+
+class LockedClass:
+    """ empty lock class """
+    __slots__ = ["first_name"]
