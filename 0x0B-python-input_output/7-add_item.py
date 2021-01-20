4565a940754f00241fc373ae5583a9edf2db2ad3
JennyHadir <hadirjenny@hotmail.com>
2021-01-20 23:28:16 +0100

    [FIX]: check 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,20 @@
-
 #!/usr/bin/python3
-""" Creates an object from json """
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""
+Script that adds all arguments to a Python list, and then saves them to a file
+"""
 
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
 
 try:
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
-items.extend(argv[1:])
-save_to_json_file(items, "add_item.json")
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
