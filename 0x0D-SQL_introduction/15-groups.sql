aa2b6df4e97fd25527e3adbb71c9600562a80110
JennyHadir <hadirjenny@hotmail.com>
2021-03-01 14:34:33 +0100

    [FIX]: from to FROM

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- List the number of records with the same score in the table
-SELECT score, COUNT(*) as number from second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
