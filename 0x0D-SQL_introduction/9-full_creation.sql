db48a372a9b6d2722c15775517519f83091a6a5c
JennyHadir <hadirjenny@hotmail.com>
2021-03-01 14:31:35 +0100

    [FIx]: last check

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Full creation of table
-CREATE TABLE IF NOT EXISTS second_table(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256), score INT);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (NULL, "John", 10);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (NULL, "Alex", 3);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (NULL, "Bob", 14);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (NULL, "George", 8);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (4, "George", 8);
