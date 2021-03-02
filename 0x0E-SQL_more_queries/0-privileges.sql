44cbb73eb0c7667777bfe18f4c08600af72e24ce
JennyHadir <hadirjenny@hotmail.com>
2021-03-02 22:24:55 +0100

    [Fix]

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Display all privileges of mysql
+-- Display all privileges of mysql users
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Display cities in a database
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id;
