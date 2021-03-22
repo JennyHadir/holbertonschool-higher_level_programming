744ceb2fd796cef5af1efe505bcf41f882ceb96b
JennyHadir <hadirjenny@hotmail.com>
2021-03-22 23:45:41 +0100

    [Fix]: last try

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 let max = 0;
-const args = process.argv.map(Number).process.argv.slice(2, process.argv.length).sort();
+const args = process.argv.slice(2);
 if (process.argv.length > 1) {
+  args.sort();
   max = args[args.length - 2];
 }
 console.log(max);
