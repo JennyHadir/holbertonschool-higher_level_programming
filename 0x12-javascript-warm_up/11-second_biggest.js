02fb2576d71a50a6e16ded8f9bbee83bfbb1ba94
JennyHadir <hadirjenny@hotmail.com>
2021-03-22 23:38:05 +0100

    [Fix]: parseInt

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let max = 0;
-const args = process.argv.slice(2);
+const args = process.argv.map(Number).process.argv.slice(2);
 if (process.argv.length > 1) {
   args.sort();
   max = args[args.length - 2];
