bc66fc6ffbab383f227d12bf805b6beac3278d59
JennyHadir <hadirjenny@hotmail.com>
2021-04-01 12:07:14 +0100

    [add]

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306, charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM WHERE name REGEXP '^N' ORDER BY id ASC")
+    cur.execute("SELECT * FROM WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
             if row[1][0] == 'N':
