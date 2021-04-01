6ce254fbabb6ff267f7a0c3e1baeefc22a97c290
JennyHadir <hadirjenny@hotmail.com>
2021-04-01 13:06:45 +0100

    fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.`id`, cities.`name`, states.`name` FROM cities\
-                 JOIN states ON citis.states_`id` = states.`id`\
-                 ORDER BY cities.`id` ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id")
     cities = cur.fetchall()
     for city in cities:
         print(city)
