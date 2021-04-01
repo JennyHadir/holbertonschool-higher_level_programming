bddf5c291815d615d6111bb2b6530ee403d98a7c
JennyHadir <hadirjenny@hotmail.com>
2021-04-01 13:44:19 +0100

    fix pep8, documentation

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,10 +6,12 @@ from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.name.contains('a')).order_by(State.id).all:
+    for state in session.query(State).filter(
+            State.name.contains('a')).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
