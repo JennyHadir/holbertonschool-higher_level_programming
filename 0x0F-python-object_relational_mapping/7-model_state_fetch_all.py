95176f64b9ab59581e8caeaea5b937c267990af7
JennyHadir <hadirjenny@hotmail.com>
2021-04-01 13:36:16 +0100

    add

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,12 +19,11 @@ if __name__ == "__main__":
     from sys import argv
     from sqlalchemy import create_engine
     from sqlalchemy.orm.session import sessionmaker
-    import sqlalchemy.orm
 
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]))
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        for state in session.query(State).order_by(State.id):
-            print("{}: {}".format(state.id, state.name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
     session.close()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,11 +8,11 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        state = session.query(State).order_by(State.id)
-        if state:
-            print("{}: {}".format(state.id, state.name))
-        else:
-            print("Nothing")
-        session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id)
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
