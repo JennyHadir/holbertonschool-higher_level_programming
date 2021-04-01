06c07f25a83fbe4cae08406edb214cf9879c2f20
JennyHadir <hadirjenny@hotmail.com>
2021-04-01 12:48:43 +0100

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """ Lists all states from a database """
-import MySQLdb
-from sys import argv
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306, charset="utf8")
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM WHERE name LIKE %s ORDER BY id ASC",(argv[4],))
+    cur.execute("SELECT * FROM states\
+                 WHERE name LIKE %s\
+                 ORDER BY id ASC", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
-            print (row)
+        print(row)
     cur.close()
     db.close()
