b6ca1fb4285642cabed4b6f4494bf77fe4ca1558
JennyHadir <hadirjenny@hotmail.com>
2021-04-12 12:16:32 +0100

    [ADD]

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Takes in a URL and an email,
  sends a POST request to the passed URL with the email as a parameter,
- and displays the body of the response (decoded in utf-8) """
+ and display the body of the response (decoded in utf-8) """
 if __name__ == "__main__":
     import urllib.request
     import urllib.parse
