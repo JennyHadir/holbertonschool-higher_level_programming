7ffa2031e2c306461dd86efd8c68d596b4aefec6
JennyHadir <hadirjenny@hotmail.com>
2021-05-04 23:30:46 +0100

    ADD

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* File system object  */
-let url = 'https://swapi-api.hbtn.io/api/films/';
+const url = 'https://swapi-api.hbtn.io/api/films/';
 const request = require('request');
 request(url + process.argv[2], function (error, response, body) {
   console.log(error || JSON.parse(body).title);
