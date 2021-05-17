96a11ef42eb174abdf9fcf86dbd72c41f48a38a9
JennyHadir <hadirjenny@hotmail.com>
2021-05-17 23:59:55 +0100

    ADD

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,8 +3,10 @@
  and uses the GitHub API to display your id"""
 if __name__ == '__main__':
     import requests
-    from requests.auth import HTTPBasicAuth
     from sys import argv
-    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    r = requests.get('https://api.github.com/user',
+                     auth=(argv[1], argv[2]))
+    try:
+        print(r.json().get['id'])
+    except Exception:
+        print('None')
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,5 +10,6 @@ if __name__ == '__main__':
                      .format(argv[2], argv[1]))
     commits = r.json()
     for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+        print("{}: {}"
+             .format(commit.get('sha'),
+              commit.get('commit').get('author').get('name')))
