e2e69c5fb688f85851c46b1c79127db1798b4e04
JennyHadir <hadirjenny@hotmail.com>
2021-05-17 23:01:26 +0100

    ADD

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 let max = 0;
 const args = process.argv.slice(2);
-if (process.argv.length > 1) {
-  args.sort();
+if (process.argv.length > 3) {
+  args.sort((a, b) => a - b);
   max = args[args.length - 2];
 }
 console.log(max);
